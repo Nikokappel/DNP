6aba22f5f72a3f45d019cbe055e81d343090b3d0
nikokappel <nikokappel@hotmail.dk>
2023-10-31 11:26:32 +0100

    updt

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasm;
+using BlazorWasm.Services;
+using BlazorWasm.Services.HTTP;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<IAuthService, JwtAuthService>();
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
