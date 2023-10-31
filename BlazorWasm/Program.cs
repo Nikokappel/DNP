79a345c6f720bb19b7f3cf7f988818d0188c7507
nikokappel <nikokappel@hotmail.dk>
2023-10-31 11:34:32 +0100

    blazor login first

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -1,13 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWasm;
+using BlazorWasm.Auth;
 using BlazorWasm.Services;
 using BlazorWasm.Services.HTTP;
+using Microsoft.AspNetCore.Components.Authorization;
+using Shared.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<IAuthService, JwtAuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+AuthorizationPolicies.AddPolicies(builder.Services);
+
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
